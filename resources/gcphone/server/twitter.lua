c136b5b36d02bb686b53887fc877e54055b3ca51
Franco Sanllehi <francosanllehi@gmail.com>
2020-08-08 19:17:17 -0400

    Feat(Discord-Webhook): Added new discord layout

--- resources/gcphone/server/twitter.lua
+++ resources/gcphone/server/twitter.lua
@@ -291,27 +291,22 @@ AddEventHandler('gcPhone:twitter_newTweets', function (tweet)
   local headers = {
     ['Content-Type'] = 'application/json'
   }
-  local data = {
-    ["username"] = tweet.author,
-    ["embeds"] = {
-      {
-        ["thumbnail"] = {
-          ["url"] = tweet.authorIcon
-        },
-        ["color"] = 1942002,
-        ["timestamp"] = os.date("!%Y-%m-%dT%H:%M:%SZ", tweet.time / 1000 )
-      }
-    }
-  }
 
   local isHttp = string.sub(tweet.message, 0, 7) == 'http://' or string.sub(tweet.message, 0, 8) == 'https://'
   local ext = string.sub(tweet.message, -4)
   local isImg = ext == '.png' or ext == '.pjg' or ext == '.gif' or string.sub(tweet.message, -5) == '.jpeg'
 
-  if (isHttp and isImg) and true then
-    data['embeds'][1]['image'] = { ['url'] = tweet.message }
-  else
-    data['embeds'][1]['description'] = tweet.message
-  end
-  PerformHttpRequest(discord_webhook, function(err, text, headers) end, 'POST', json.encode(data), headers)
+  local data = {
+    {
+        ["color"] = "1942002",
+        ["title"] = _U('new_tweet'),
+        ["description"] = tweet.message,
+        ["footer"] = {
+            ["text"] = tweet.author,
+            ["icon_url"] = tweet.authorIcon,
+        },
+    }
+  }
+
+  PerformHttpRequest(discord_webhook, function(err, text, headers) end, 'POST', PerformHttpRequest(discord_webhook, function(err, text, headers) print(err) end, 'POST', json.encode({username = "Twitter", embeds = data}), headers), headers)
 end)
