0c31c2d5c67b1ec1ecd02616a3578e1748954329
Kanin Peanviriyakulkit <dogrocker@users.noreply.github.com>
2020-10-06 11:15:10 +0700

    Add th lang

---
+++ resources/gcphone/locales/th.lua
@@ -0,0 +1,12 @@
+Locales['th'] = {
+    ["mission_row"] = "สถานีตำรวจ",
+    ["phone_booth"] = "ตู้โทรศัพท์",
+    ["key_answer"] = "กด ~INPUT_PICKUP~ เพื่อรับโทรศัพท์",
+    ["new_message"] = "~o~ข้อความใหม่",
+    ["new_message_from"] = "~o~ข้อความใหม่จาก ~y~%s",
+    ["new_message_transmitter"] = "~o~ข้อความใหม่จาก ~g~%s",
+    ["use_fixed"] = "~g~%s's phone ~o~(%s) ~n~~INPUT_PICKUP~~w~ Use phone",
+    
+    -- Social
+    ["new_tweet"] = "Tweet ใหม่!"
+}
